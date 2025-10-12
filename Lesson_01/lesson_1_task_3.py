c9207280086af92a6511b1cda68967b1ceaf12b4
Lora870 <rionataly@yandex.ru>
2025-10-13 01:34:02 +0500

    Lesson_1

---
+++ Lesson_01/lesson_1_task_3.py
@@ -0,0 +1,5 @@
+first_name = "Наталья"
+last_name = "Позднякова"
+input
+print("Вас зовут: " + last_name + " " + first_name)
+
---
+++ Lesson_01/lesson_1_task_5.py
@@ -0,0 +1,7 @@
+def print_num(num):
+    print(num, end='')
+
+номер = "88005553535"
+for цифра in номер:
+    print_num(цифра)
+print()
