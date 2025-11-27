5d45b2a9cf555aff20c0aa72a99db99485381efa
Lora870 <rionataly@yandex.ru>
2025-11-27 21:02:23 +0500

    задание №6 (1,2 -исправления)

--- Lesson_06/lesson_06_task_1.py
+++ Lesson_06/lesson_06_task_1.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 # Шаг 1: Настройка драйвера Chrome
@@ -18,19 +16,12 @@ try:
     button = driver.find_element(By.CSS_SELECTOR, '.btn-primary')
     button.click()
     print("Кнопка кликнута! ⏳ Ждём AJAX...")
-
-    # Шаг 4: Ждём появления зелёной плашки (класс ajax-content или success-msg)
-    wait = WebDriverWait(driver, 10)  # До 10 сек
-    green_banner = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ajax-content.success-msg')))  # Или.alert-success, проверь F12
-
-    # Шаг 5: Получаем и выводим текст
-    text = green_banner.text
-    print(f"Текст из зелёной плашки: {text}")
-
+    driver.implicitly_wait(20)
+    # Шаг 4: Ждём появления зелёной плашки и печатаем
+    content = driver.find_element(By.CSS_SELECTOR, "#content")
+    txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
+    print(txt)
     sleep(2)  # Пауза для просмотра
-
-except Exception as e:
-    print(f"Ошибка: {e}")
 
 finally:
     driver.quit()
--- Lesson_06/lesson_06_task_2.py
+++ Lesson_06/lesson_06_task_2.py
@@ -9,13 +9,13 @@ from time import sleep
 # Шаг 1: Настройка драйвера Chrome
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
-
+driver.implicitly_wait(20)
 try:
     # Шаг 2: Переходим на страницу
     driver.get('http://uitestingplayground.com/textinput')
 
     # Шаг 3: Находим поле ввода (id="my-input" или.form-control)
-    input_field = driver.find_element(By.ID, 'my-input')
+    input_field = driver.find_element(By.CSS_SELECTOR, '#newButtonName')
 
     # Шаг 4: Вводим текст "SkyPro"
     input_field.clear()  # Очищаем, если нужно
@@ -23,16 +23,14 @@ try:
     print("Текст 'SkyPro' введён в поле! ✅")
 
     # Шаг 5: Кликаем по синей кнопке
-    button = driver.find_element(By.CSS_SELECTOR, '.btn-primary')
+    button = driver.find_element(By.CSS_SELECTOR, "#updatingButton")
     button.click()
     print("Кнопка кликнута! ⏳ Ждём обновления...")
 
     # Шаг 6: Ждём изменения кнопки и получаем текст
-    wait = WebDriverWait(driver, 10)
-    button = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.btn-primary'), 'SkyPro'))
-    button_text = button.text
-    print(f"Текст кнопки: '{button_text}'")  # Вывод: "SkyPro"
-
+    # driver.implicitly_wait(20)
+    txt = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
+    print(txt)
     sleep(2)  # Пауза для просмотра
 
 except Exception as e:
